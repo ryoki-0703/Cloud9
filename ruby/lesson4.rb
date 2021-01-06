09a46943f4a4d98a46c2ced39ec879aa13a3f85c
EC2 Default User <ec2-user@ip-172-31-57-219.ec2.internal>
2021-01-06 09:12:16 +0000

    first commit

---
+++ ruby/lesson4.rb
@@ -0,0 +1,2 @@
+name = "塩田椋葵"
+puts name
---
+++ ruby/lesson7-2.rb
@@ -0,0 +1,30 @@
+puts "計算をはじめます"
+puts "何回計算を繰り返しますか？"
+
+ab = gets.to_i
+i = 1
+while i <= ab do
+puts "#{i}回目の計算"
+puts "2つの値を入力してください"
+
+a = gets.to_i
+b = gets.to_i
+
+puts "a=#{a}"
+puts "b=#{b}"
+
+puts "計算結果を出力します"
+puts "a+B=#{a + b}"
+puts "a-b-#{a - b }"
+puts "a*b=#{a * b}"
+puts "a/b=#{a / b}"
+
+i += 1
+end
+
+puts "計算を終了します"
+
+
+
+
+

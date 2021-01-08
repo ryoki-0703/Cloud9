7d5d269b9a4838d340478dd10ee74e7c84f7238c
EC2 Default User <ec2-user@ip-172-31-57-219.ec2.internal>
2021-01-08 06:40:00 +0000

    [Finish] 5-10

--- rails/app/controllers/todolists_controller.rb
+++ rails/app/controllers/todolists_controller.rb
@@ -30,7 +30,10 @@ class TodolistsController < ApplicationController
   end
   
   def destroy
-  	
+  	list = List.find(params[:id])
+  	list.destroy
+  	redirect_to todolists_path
+  end
 
  private
 
